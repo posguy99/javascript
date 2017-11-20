62c78638913fee95e6cd1ec897478d89684cb9be
Marc Wilson <posguy99@gmail.com>
2017-11-20 12:29:19 -0800

    Added Notification Center pop

--- Fluid/owa-dockbadge-notify.js
+++ Fluid/owa-dockbadge-notify.js
@@ -1,75 +1,87 @@
-// Userscript for Fluid App to enable dock badge and desktop notifications for SeaWorld OWA
-// Marc Wilson posguy99@gmail.com
+// use for patterns:
+// https://login.microsoftonline.com/*
+// https://outlook.office365.com/*
 
-// based on:
-//
-// Userscript for Fluid App to enable dock badge and desktop notifications for Fastmail.
-//
-// Author: @lylo
-// https://gist.github.com/lylo/7786717
+// desktop badge count for https://outlook.office365.com/
 
-window.fluid.dockBadge = '';
+(function () {
+    var logging = true;
+    var previousBadge = 0;
 
-// for tracking when to notify
-var previousUnreadCount = 0;
+    function log () {
+        return logging && console.log.apply(console, ['badge:'].concat(Array.prototype.slice.apply(arguments)));
+    }
 
-// Main polling
-var readyStateCheckInterval = setInterval(function() {
-  if(document.readyState === "complete") {
-    setInterval(updateDockBadge, 1000);
-    clearInterval(readyStateCheckInterval);
-  }
-}, 100);
+    function getBadge() {
+        return window.fluid.dockBadge;
+    }
 
+    function setBadge(val) {
+        window.fluid.dockBadge = val;
+    }
 
-function updateDockBadge() {
-  var currentUnreadCount = 0;
+    function setOrRemoveBadge(val) {
+        if (!val) {
+            val = undefined;
+        }
 
-  var tree = document.getElementsByClassName("v-FolderTree")[0];
-  for(i = 0; i < tree.childNodes.length; ++i) {
-    name = tree.childNodes[i].getElementsByClassName("v-FolderSource-name")[0].innerText;
-    badge = tree.childNodes[i].getElementsByClassName("v-FolderSource-badge")[0].innerText;
+        return setBadge(val);
+    }
 
-    // if you only want to report on the Inbox, change the following line to
-    // if (badge && name && name == "Inbox")
-    if (badge && name && name == "Inbox") {
-      if(parseInt(badge) > 0) {
-        currentUnreadCount += parseInt(badge);
+    function extractCountFromUnreadText(text, fallback) {
+        var res = text.match(/\s*\d+\s*/);
+
+        if (res && res.length > 0) {
+            return parseInt(res[0].trim(), 10);
+        }
+
+        return fallback;
+    }
+
+    // Creates a Notification Center message, if supported
+    function notify(count) {
+      var supportsWebkitNotifications = ('webkitNotifications' in window);
+
+      if(count > 0 && supportsWebkitNotifications) {
+        if(webkitNotifications.checkPermission() == 0) {
+          body = (count == 1) ? '1 new message' : count + ' new messages';
+
+          webkitNotifications.createNotification(
+            null,
+            'You got mail',
+            body
+          ).show();
+        }
       }
     }
-  }
 
-  if(currentUnreadCount == 0) {
-    window.fluid.dockBadge = '';
-    previousUnreadCount = 0;
-  } else {
-    // update dock with new count
-    window.fluid.dockBadge = currentUnreadCount;
+    function check() {
+        log('checking...')
 
-    // determine how many new mails there are and notify
-    var unreadMailCount = currentUnreadCount - previousUnreadCount;
-    if(unreadMailCount > 0) {
-      notify(unreadMailCount);
+        var loginPanel = document.querySelector('[id="login_panel"]');
 
-      previousUnreadCount = currentUnreadCount;
-    }
-  }
-}
+        if (loginPanel) {
+            setBadge('!');
+        } else {
+            // attempt to locate the unread count
+            var favouriteTree = document.querySelector('[id="MailFolderPane.FavoritesFolders"]');
+            var firstFavourite = favouriteTree && favouriteTree.querySelector('[role=treeitem]');
+            var unreadSpan = firstFavourite && firstFavourite.querySelector('span[id$=".ucount"]');
+            var text = '' + (unreadSpan || {}).innerText;
+            var newBadge = extractCountFromUnreadText(text, 0);
+            // newBadge should always be an integer here
 
+            setOrRemoveBadge(newBadge);
 
-// Creates a Notification Center message, if supported
-function notify(count) {
-  var supportsWebkitNotifications = ('webkitNotifications' in window);
+            // determine how many new mails there are and notify as necessary
+            var unreadMailCount = newBadge - previousBadge;
+            if(unreadMailCount > 0) {
+              notify(unreadMailCount);
+              previousBadge = newBadge;
+          }
+        }
 
-  if(count > 0 && supportsWebkitNotifications) {
-    if(webkitNotifications.checkPermission() == 0) {
-      body = (count == 1) ? '1 new message' : count + ' new messages';
-
-      webkitNotifications.createNotification(
-        null,
-        'You got mail',
-        body
-      ).show();
-    }
-  }
-}
+        window.setTimeout(check, 5000);
+    };
+    check();
+})();
