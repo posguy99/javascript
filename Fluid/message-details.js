8e4a3b156d0e74a356d3a46497925d1881e22b23
Marc Wilson <posguy99@gmail.com>
2018-09-09 12:54:15 -0700

    Initial commit.

---
+++ Fluid/message-details.js
@@ -0,0 +1,5 @@
+// https://openuserjs.org/scripts/st0n3/Fastmail_-_always_show_email_details
+
+var style = document.createElement("style");
+style.innerHTML = ".v-Message-details { height: auto; } .v-MessageCard-toggleDetails {display:none;}";
+document.body.appendChild(style);
